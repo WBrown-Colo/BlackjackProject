4f4dee86dd85ea8543e8086749d7ad9e840cd3a4
William Brown <wsbrown.colo@gmail.com>
2024-11-17 16:00:37 -0700

    establish playGame(), draft class layout in app

--- src/com/skilldistillery/cards/app/BlackjackApp.java
+++ src/com/skilldistillery/cards/app/BlackjackApp.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 import com.skilldistillery.cards.blackjack.Dealer;
 import com.skilldistillery.cards.blackjack.Player;
+import com.skilldistillery.cards.blackjack.BlackjackHand;
 
 public class BlackjackApp {
 	
@@ -23,8 +24,48 @@ public class BlackjackApp {
 	}
 	
 	public void playGame() {
+		//INTRO
+		System.out.println("Let's play BLACKJACK!");
+		System.out.println();
+		//DEALER SHUFFLING
+		System.out.println("The dealer is shuffling the cards.");
+		dealer.shuffle();
+		//PLAYER 1ST CARD
+		player.addCardToHand(dealer.dealCard());
+		System.out.println("Your first card is: ");
+		player.displayHand();
+		System.out.println();
+		//DEALER HIDDEN CARD
+		System.out.println("The dealer's card is hidden.");
+		dealer.addCardToHand(dealer.dealCard());
+		System.out.println();
+		//PLAYER 2ND CARD
+		player.addCardToHand(dealer.dealCard());
+		System.out.println("Your second card is: ");
+		player.displayHand();
+		System.out.println();
+		//DEALER HAND
+		System.out.println("The dealer's cards are: ");
+		dealer.addCardToHand(dealer.dealCard());
+		//PLAYER HAND VALUE
+		System.out.println("Your hand value is " + player.getHandValue());
+		System.out.println();
+		
+		if (player.getHandValue() == 21) {
+			System.out.println("YOU HIT BLACKJACK!!!");
+		}
+		if (dealer.getHandValue() == 21) {
+			System.out.println("SORRY, THE DEALER HIT BLACKJACK.");
+		}
+		else {
+//			hitOrStand();
+		}
 		
 	}
+	
+	//create hitOrStand method
+	//create Win/Loss validation
+	//	both dealer and player	
 
 }
 // NO MORE FIELDS!!!
